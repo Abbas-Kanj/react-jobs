0ac77c61a3da77e980f47419bb768f760cf7aa71
Abbas <abbas_kanj@hotmail.com>
2024-06-22 12:59:00 +0300

    modified jobpage to use useLoaderData instead of useEffect to fetch data and render data at any page if needed

--- src/components/JobPage.jsx
+++ src/components/JobPage.jsx
@@ -1,33 +1,38 @@
 import React, { useEffect, useState } from "react";
 import Spinner from "./Spinner";
-import { useParams } from "react-router-dom";
+import { useParams, useLoaderData } from "react-router-dom";
 
 const JobPage = () => {
-  const [job, setJob] = useState(null);
-  const [loading, setLoading] = useState(true);
   const { id } = useParams();
-  useEffect(() => {
-    const fetchApi = async () => {
-      try {
-        const res = await fetch(`/api/jobs/${id}`);
-        const data = await res.json();
-        setJob(data);
-      } catch (error) {
-        console.log("Error Fetching Data: ", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchApi();
-  }, []);
+  const job = useLoaderData();
+  //   const [job, setJob] = useState(null);
+  //   const [loading, setLoading] = useState(true);
+  //   useEffect(() => {
+  //     const fetchApi = async () => {
+  //       try {
+  //         const res = await fetch(`/api/jobs/${id}`);
+  //         const data = await res.json();
+  //         setJob(data);
+  //       } catch (error) {
+  //         console.log("Error Fetching Data: ", error);
+  //       } finally {
+  //         setLoading(false);
+  //       }
+  //     };
+  //     fetchApi();
+  //   }, []);
 
-  return loading ? (
-    <Spinner />
-  ) : (
+  return (
     <>
       <h1>{job.title}</h1>
     </>
   );
 };
 
-export default JobPage;
+const jobLoader = async ({ params }) => {
+  const res = await fetch(`/api/jobs/${params.id}`);
+  const data = res.json();
+  return data;
+};
+
+export { JobPage as default, jobLoader };
